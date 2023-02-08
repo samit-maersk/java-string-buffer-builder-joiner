bd4496deef503f70ef63fa36042b84e5857cc33a
samit-maersk <SamitKumar.Patel@maersk.com>
2023-02-08 12:13:19 +0100

    update

--- src/main/java/net/java/chapter/Java18.java
+++ src/main/java/net/java/chapter/Java18.java
@@ -1,20 +1,21 @@
 package net.java.chapter;
 
+import java.net.InetAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.ServiceLoader;
 
 /**
  * 1. JEP 400: UTF-8 by Default
  * 2. JEP 408: Simple Web Server
  * 3. JEP 413: Code Snippets in Java API Documentation
- * 4. JEP 416: Reimplement Core Reflection with Method Handles
- * 5. JEP 417: Vector API (Third Incubator)
- * 6. JEP 418: Internet-Address Resolution SPI
- * 7. JEP 419: Foreign Function & Memory API (Second Incubator)
- * 8. JEP 420: Pattern Matching for switch (Second Preview)
- * 8.1. Dominance checking of the same type.
- * 8.2 Exhaustiveness of switch expressions and statements
+ * 4. JEP 418: Internet-Address Resolution SPI
+ * 5. JEP 420: Pattern Matching for switch (Second Preview)
+ * 6. JEP 416: Reimplement Core Reflection with Method Handles
+ * 7. JEP 417: Vector API (Third Incubator)
+ * 8. JEP 419: Foreign Function & Memory API (Second Incubator)
  * 9. JEP 421: Deprecate Finalization for Removal
  */
 
@@ -22,7 +23,7 @@ import java.nio.file.Paths;
 public class Java18 {
     public static void main(String[] args) throws Exception {
 
-        // 1.) Charset = UTF-8
+        // 1.) JEP 400: UTF-8 by Default
         /** In Java 18, the default encoding standard charset to UTF-8. However, it's still allow configuring the default charset to others by providing the system property ‘file.encoding’. `java -Dfile.encoding=COMPAT`
          *
          * Before Java 18, the default charset was environment-dependent, meaning the Java Virtual Machine (JVM) chooses the default charset during start-up,
@@ -39,7 +40,7 @@ public class Java18 {
         String text = Files.readString(path);
         System.out.println(text);*/
 
-        //2.) jwebserver
+        //2.) JEP 408: Simple Web Server (jwebserver)
         /**
          * Provides a command-line tool, jwebserver to start a simple web server to serve static files, suitable for prototyping, ad-hoc coding, testing, and educational purpose, not for a production server.
          * It has some limitation
@@ -48,7 +49,7 @@ public class Java18 {
          * Only the HTTP GET and HEAD methods are allowed.
          */
 
-        // 3.) Code Snippets in Java API Documentation
+        // 3.) JEP 413: Code Snippets in Java API Documentation
         /**
          * OLD
          * need to start with <pre>
@@ -69,84 +70,76 @@ public class Java18 {
          * Integrate Snippets from Other Files
          */
 
+        // 4.) JEP 418: Internet-Address Resolution SPI
+        /**
+         * Since 1.0
+         * To find out the IP address(es) for a hostname in Java, we can use InetAddress.getByName(…) or InetAddress.getAllByName(…).
+         */
+        /*
+        var allByName = InetAddress.getAllByName("www.maersk.com");
+        Arrays.stream(allByName).forEach(System.out::println);
+        //For reverse lookups (i.e., resolving an IP address to a hostname), the JDK provides the methods
+        // InetAddress::getCanonicalHostName and InetAddress::getHostName.
+        */
+
+        /**
+         * By default, InetAddress uses the operating system's resolver, i.e., it usually consults the hosts file and the configured DNS servers.
+         *
+         * In this release java.net.InetAddress API to use service loader to find the resolver instead of using the operating system’s built-in resolver (InetAddress::loadResolver)
+         * private static InetAddressResolver loadResolver() {
+         *       return ServiceLoader.load(InetAddressResolverProvider.class)
+         *               .findFirst()
+         *               .map(nsp -> nsp.get(builtinConfiguration()))
+         *               .orElse(BUILTIN_RESOLVER);
+         *   }
+         */
+
+        // 5.) Pattern Matching for switch (Second Preview)
+        /**
+         * "Pattern Matching for switch" was first introduced in Java 17 and enables switch statements (and expressions) such as following
+         */
+
+        /*
+        switch ((Object)"foobar") {
+            case String s && s.length() > 5 -> System.out.println(s.toUpperCase());
+            case String s                   -> System.out.println(s.toLowerCase());
+            case Integer i                  -> System.out.println(i * i);
+            default -> {}
+        }
+
+        // Java 17
+        switch ("foobar") { //Label is dominated by a preceding case label 'String s && s.length() > 5'
+            case String s && s.length() > 5 -> System.out.println(s.toUpperCase());
+            case CONSTANT                   -> System.out.println("baz");
+            default -> {}
+        }
+
+        // Java 18
+        switch ("foobar") { //Label is dominated by a preceding case label 'String s && s.length() > 5'
+            case String s && s.length() > 5 -> System.out.println(s.toUpperCase());
+            case CONSTANT                   -> System.out.println("baz");
+            default -> {}
+        }
+
+        switch (o) { // Error - pattern is dominated by previous pattern
+            case CharSequence cs -> System.out.println("A sequence of length " + cs.length());
+            case String s ->
+                    System.out.println("A string: " + s);
+            default -> {
+                break;
+            }
+        }
+
+        switch (o) {         // Error - not exhaustive
+            case String s  -> s.length();
+            case Integer i -> i;
+        }
+
+        */
+
     }
 
 
-
-
-
-
-    // 3.) Code Snippets in Java API Documentation
-    /**
-     * <pre>{@code
-     *     int sum = widgets.stream()
-     *                      .filter(w -> w.getColor() == RED)
-     *                      .mapToInt(w -> w.getWeight())
-     *                      .sum();
-     * }</pre>
-     */
-
-    //Inline snippets
-    /**
-     * The following code shows how to use {@code Optional.isPresent}:
-     * {@snippet :
-     * if (v.isPresent()) {
-     *     System.out.println("v: " + v.get());
-     * }
-     * }
-     */
-
-    //External snippets
-    /**
-     * The following code shows how to use {@code Optional.isPresent}:
-     * {@snippet file="ShowOptional.java" region="example"}
-     */
-    /*
-        public class ShowOptional {
-            void show(Optional<String> v) {
-                // @start region="example"
-                if (v.isPresent()) {
-                    System.out.println("v: " + v.get());
-                }
-                // @end
-            }
-        }
-     */
-
-
-    // 4.) Reimplement Core Reflection with Method Handles
-    /**
-     * We can enable back the old implementation via -Djdk.reflect.useDirectMethodHandle=false.
-     */
-
-    // 5.) Vector API (Third Incubator)
-    /**
-     * Java 16, JEP 338 introduced new Vector API as an incubating API.
-     * Java 17, JEP 414 enhanced the Vector API second incubator.
-     *
-     */
-
-    // 6.) Internet-Address Resolution SPI
-    /**
-     * By default, the java.net.InetAddress API uses the operating system’s built-in resolver to resolve host names to Internet Protocol (IP) addresses
-     * In this release java.net.InetAddress API to use service loader to find the resolver instead of using the operating system’s built-in resolver.
-     *
-     * InetAddress ip = InetAddress.getByName("google.com");
-     *
-     * private static InetAddressResolver loadResolver() {
-     *       return ServiceLoader.load(InetAddressResolverProvider.class)
-     *               .findFirst()
-     *               .map(nsp -> nsp.get(builtinConfiguration()))
-     *               .orElse(BUILTIN_RESOLVER);
-     *  }
-     */
-
-    // 7.) Foreign Function & Memory API (Second Incubator)
-    /**
-     * This Foreign Function & Memory API allows the developer to access the code outside the JVM (foreign functions), data stored outside the JVM (off-heap data), and accessing memory not managed by the JVM (foreign memory).
-     *
-     * This JEP improves the Foreign Function & Memory API and other enhancements
-     */
 
     // 8.) Pattern Matching for switch
 
